c26c9858cfc721f03fa27d3be4931df4290d57b8
huyang <huyang@changba.com>
2020-07-24 14:41:58 +0800

    Initial Commit

---
+++ WireShark/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WireShark
+//
+//  Created by huyang on 2020/7/24.
+//  Copyright © 2020 huyang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
